65d41227a40aa89b66a6de3a8be1571ea0ef5448
exoticswingset <58611901+exoticswingset@users.noreply.github.com>
2019-12-22 00:27:44 -0500

    Added TruthfulWifiDisable

--- packages.sh
+++ packages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/TruthfulDock debs
+dpkg-deb -bZgzip projects/TruthfulWifiDisable debs
 dpkg-deb -bZgzip projects/TruthfulFolders debs
 dpkg-deb -bZgzip projects/DualSBAdoption debs
 # dpkg-deb -bZgzip projects/<project name> <output folder>
