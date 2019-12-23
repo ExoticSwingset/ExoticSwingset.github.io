d28cb28bbb36ec19fd96b48484bac376230576da
exoticswingset <58611901+exoticswingset@users.noreply.github.com>
2019-12-22 23:49:23 -0500

    Added TruthfulBTDisable

--- packages.sh
+++ packages.sh
@@ -3,4 +3,5 @@ dpkg-deb -bZgzip projects/TruthfulDock debs
 dpkg-deb -bZgzip projects/TruthfulWifiDisable debs
 dpkg-deb -bZgzip projects/TruthfulFolders debs
 dpkg-deb -bZgzip projects/DualSBAdoption debs
+dpkg-deb -bZgzip projects/TruthfulBTDisable debs
 # dpkg-deb -bZgzip projects/<project name> <output folder>
