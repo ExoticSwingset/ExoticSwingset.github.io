333ac00dfa7ccb364f3dbe6f98bf16ee2cadddb2
exoticswingset <58611901+exoticswingset@users.noreply.github.com>
2019-12-23 13:46:22 -0500

    Added StopAStupidThief

--- packages.sh
+++ packages.sh
@@ -4,4 +4,5 @@ dpkg-deb -bZgzip projects/TruthfulWifiDisable debs
 dpkg-deb -bZgzip projects/TruthfulFolders debs
 dpkg-deb -bZgzip projects/DualSBAdoption debs
 dpkg-deb -bZgzip projects/TruthfulBTDisable debs
+dpkg-deb -bZgzip projects/StopAStupidThief debs
 # dpkg-deb -bZgzip projects/<project name> <output folder>
