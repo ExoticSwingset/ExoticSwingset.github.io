aeefb4b25e8185485e049681bbe7949d81ffa903
exoticswingset <58611901+exoticswingset@users.noreply.github.com>
2019-12-23 20:04:41 -0500

    Added DualLSButtonAdoption

--- packages.sh
+++ packages.sh
@@ -5,4 +5,5 @@ dpkg-deb -bZgzip projects/TruthfulFolders debs
 dpkg-deb -bZgzip projects/DualSBAdoption debs
 dpkg-deb -bZgzip projects/TruthfulBTDisable debs
 dpkg-deb -bZgzip projects/StopAStupidThief debs
+dpkg-deb -bZgzip projects/DualLSButtonAdoption debs
 # dpkg-deb -bZgzip projects/<project name> <output folder>
