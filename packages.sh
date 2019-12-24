5dfa85bb18e069c00e959bddcc04ec7a65961fe0
exoticswingset <58611901+exoticswingset@users.noreply.github.com>
2019-12-23 21:43:49 -0500

    Added CCStatusShush

--- packages.sh
+++ packages.sh
@@ -6,4 +6,5 @@ dpkg-deb -bZgzip projects/DualSBAdoption debs
 dpkg-deb -bZgzip projects/TruthfulBTDisable debs
 dpkg-deb -bZgzip projects/StopAStupidThief debs
 dpkg-deb -bZgzip projects/DualLSButtonAdoption debs
+dpkg-deb -bZgzip projects/CCStatusShush debs
 # dpkg-deb -bZgzip projects/<project name> <output folder>
