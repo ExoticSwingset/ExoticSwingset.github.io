d2029199066e66ef73945a9521fc0157d4dec013
exoticswingset <58611901+exoticswingset@users.noreply.github.com>
2020-01-03 12:41:04 -0500

    Removed StopAStupidThief to make sure it works on ios 13 first

--- packages.sh
+++ packages.sh
@@ -4,7 +4,6 @@ dpkg-deb -bZgzip projects/TruthfulWifiDisable debs
 dpkg-deb -bZgzip projects/TruthfulFolders debs
 dpkg-deb -bZgzip projects/DualSBAdoption debs
 dpkg-deb -bZgzip projects/TruthfulBTDisable debs
-dpkg-deb -bZgzip projects/StopAStupidThief debs
 dpkg-deb -bZgzip projects/DualLSButtonAdoption debs
 dpkg-deb -bZgzip projects/CCStatusShush debs
 # dpkg-deb -bZgzip projects/<project name> <output folder>
